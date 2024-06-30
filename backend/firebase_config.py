b2b4798e7be4c02e118412906f80fb3797e391db
3smondi <pedro_tresmondi@hotmail.com>
2024-06-30 01:23:02 -0300

    adding docker-compose

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,19 +1,22 @@
 import os
+import json
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, storage, db
 
 load_dotenv()
 
-# Caminho relativo ao diretório atual de execução
-cred_path = os.path.join(os.path.dirname(__file__), os.getenv('FIREBASE_CREDENTIALS_PATH'))
-print(f"Caminho do arquivo de credenciais: {cred_path}")
+# Obtenha o JSON da variável de ambiente
+cred_json = os.getenv('FIREBASE_CREDENTIALS_JSON')
+if cred_json is None:
+    raise ValueError("A variável de ambiente FIREBASE_CREDENTIALS_JSON não está definida")
 
-database_url = os.getenv('FIREBASE_DATABASE_URL')
-storage_bucket = os.getenv('FIREBASE_STORAGE_BUCKET')
+# Parse o JSON
+cred_dict = json.loads(cred_json)
 
-cred = credentials.Certificate(cred_path)
+# Inicialize as credenciais usando o dicionário JSON
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': database_url,
-    'storageBucket': storage_bucket
+    'databaseURL': os.getenv('FIREBASE_DATABASE_URL'),
+    'storageBucket': os.getenv('FIREBASE_STORAGE_BUCKET')
 })
